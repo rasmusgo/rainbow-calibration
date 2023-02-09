6957d1f85ecaa5165ee990018d356b0e864ed855
Rasmus Brönnegård <1162652+rasmusgo@users.noreply.github.com>
2023-02-09 01:17:36 +0100

    Add button to capture camera image

--- src/app.rs
+++ src/app.rs
@@ -1,30 +1,22 @@
-use crate::calibration_pattern::generate_calibration_pattern;
+use image::{ImageBuffer, Rgb};
+
+use crate::{calibration_pattern::generate_calibration_pattern, camera::capture_camera_image};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct App {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
     #[serde(skip)]
-    value: f32,
-
+    camera_image: Option<ImageBuffer<Rgb<u8>, Vec<u8>>>,
     #[serde(skip)]
     texture: Option<egui::TextureHandle>,
-    #[serde(skip)]
-    texture_size: Option<(u32, u32)>,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            camera_image: Default::default(),
             texture: Default::default(),
-            texture_size: Default::default(),
         }
     }
 }
@@ -55,23 +47,20 @@ impl eframe::App for App {
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
-            label,
-            value,
+            camera_image,
             texture,
-            texture_size,
         } = self;
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Camera controls");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
+            if ui.button("Capture camera image").clicked() {
+                *camera_image = Some(capture_camera_image());
+                *texture = None;
+            }
+            if ui.button("Reset image").clicked() {
+                *camera_image = None;
+                *texture = None;
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
@@ -91,33 +80,49 @@ impl eframe::App for App {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            let (img_width, img_height) = if ui.available_size().is_finite() {
-                (ui.available_width() as _, ui.available_height() as _)
+            if let Some(camera_image) = camera_image {
+                let texture: &egui::TextureHandle = texture.get_or_insert_with(|| {
+                    // if let Some(state) =_frame.wgpu_render_state() {
+                    //     state.renderer.write().register_native_texture(device, texture, texture_filter)
+                    // }
+                    let size = [camera_image.width() as _, camera_image.height() as _];
+                    let pixels = camera_image.as_flat_samples();
+                    ui.ctx().load_texture(
+                        "camera-image",
+                        egui::ColorImage::from_rgb(size, pixels.as_slice()),
+                        Default::default(),
+                    )
+                });
+                // Show the image:
+                ui.image(texture, texture.size_vec2());
             } else {
-                (1280, 1280)
-            };
-            if let Some(texture_size) = texture_size {
-                if img_width != texture_size.0 || img_height != texture_size.1 {
-                    *texture = None;
+                let (img_width, img_height) = if ui.available_size().is_finite() {
+                    (ui.available_width() as _, ui.available_height() as _)
+                } else {
+                    (1280, 1280)
+                };
+                if let Some(t) = texture {
+                    let [texture_width, texture_height] = t.size();
+                    if img_width != texture_width as _ || img_height != texture_height as _ {
+                        *texture = None;
+                    }
                 }
+                let texture: &egui::TextureHandle = texture.get_or_insert_with(|| {
+                    // if let Some(state) =_frame.wgpu_render_state() {
+                    //     state.renderer.write().register_native_texture(device, texture, texture_filter)
+                    // }
+                    let image_buffer = generate_calibration_pattern(img_width, img_height);
+                    let size = [image_buffer.width() as _, image_buffer.height() as _];
+                    let pixels = image_buffer.as_flat_samples();
+                    ui.ctx().load_texture(
+                        "calibration-patter-image",
+                        egui::ColorImage::from_rgb(size, pixels.as_slice()),
+                        Default::default(),
+                    )
+                });
+                // Show the image:
+                ui.image(texture, texture.size_vec2());
             }
-            let texture: &egui::TextureHandle = texture.get_or_insert_with(|| {
-                *texture_size = Some((img_width, img_height));
-                // if let Some(state) =_frame.wgpu_render_state() {
-                //     state.renderer.write().register_native_texture(device, texture, texture_filter)
-                // }
-                let image_buffer = generate_calibration_pattern(img_width, img_height);
-                let size = [image_buffer.width() as _, image_buffer.height() as _];
-                let pixels = image_buffer.as_flat_samples();
-                ui.ctx().load_texture(
-                    "my-image",
-                    egui::ColorImage::from_rgb(size, pixels.as_slice()),
-                    Default::default(),
-                )
-            });
-
-            // Show the image:
-            ui.image(texture, texture.size_vec2());
         });
     }
 }
--- src/camera.rs
+++ src/camera.rs
@@ -1,10 +1,11 @@
+use image::{ImageBuffer, Rgb};
 use nokhwa::{
     pixel_format::RgbFormat,
     utils::{CameraIndex, RequestedFormat, RequestedFormatType},
     Camera,
 };
 
-pub fn capture_camera_image() {
+pub fn capture_camera_image() -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     // first camera in system
     let index = CameraIndex::Index(0);
     // request the absolute highest resolution CameraFormat that can be decoded to RGB.
@@ -20,5 +21,6 @@ pub fn capture_camera_image() {
     let decoded = frame.decode_image::<RgbFormat>().unwrap();
     println!("Decoded Frame of {}", decoded.len());
 
-    decoded.save("camera_image.png").unwrap();
+    // decoded.save("camera_image.png").unwrap();
+    decoded
 }
